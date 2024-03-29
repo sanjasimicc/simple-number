1609c463472f2b3123178443e1103e4983c81b72
Sanja Simic <sanja.simic@sigmoid.rs>
2024-03-29 10:16:44 +0100

    first commit

---
+++ SimpleNumber/SimpleNumber.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.RegularExpressions;
+
+namespace SimpleNumber;
+
+public class SimpleNumber
+{
+
+     private static readonly string characterSequence = "2024. godine će se održati 6. svetsko prvenstvo u boćanju.";
+     private static int CountSimpleNumbers(string characterSequence) {
+         string pattern = @"\D\d\D";
+          Regex regex = new Regex(pattern, RegexOptions.IgnoreCase | RegexOptions.CultureInvariant);
+          MatchCollection matches = regex.Matches(characterSequence);
+         return matches.Count();
+         
+     }
+
+    public static void main(String[] args) {
+       int count = CountSimpleNumbers(characterSequence); 
+       Console.WriteLine(count);
+    }
+ }
+
+
